f6bd515fca354641c1d5dcd642de244c670c321e
Oka <45233151+OkaCola@users.noreply.github.com>
2021-11-26 14:22:02 +0100

    example2

--- methodo/settings.py
+++ methodo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jqrcthu1v8tu-x(gum^^h6jrfyt@!v)92*oil+(j@%p8$5t8^r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "0.0.0.0", "127.0.0.1","7.7.7.7"]
+ALLOWED_HOSTS = [ "0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
